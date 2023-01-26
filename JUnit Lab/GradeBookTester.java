abf42d6ceffefb5396b3bf82b6865e9db5ced7ea
cclifton2023 <92695770+cclifton2023@users.noreply.github.com>
2023-01-26 11:32:15 -0500

    Add files via upload

---
+++ JUnit Lab/GradeBookTester.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GradeBookTester 
+{
+	GradeBook g1;
+
+	@BeforeEach
+	void setUp() throws Exception 
+	{
+		g1 = new GradeBook(5);
+		g1.addScore(50);
+		g1.addScore(75);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception 
+	{
+		g1 = null;
+	}
+
+	@Test
+	void testAddScore() 
+	{
+		assertTrue(g1.toString().equals("50.0 75.0 "));
+		
+	}
+	@Test
+	void testSum() 
+	{
+		assertEquals(125, g1.sum(), .0001);
+	}
+	@Test
+	void testMinimum() 
+	{
+		assertEquals(50, g1.minimum(), .001);
+	}
+	@Test
+	void testFinalScore() 
+	{
+		assertEquals(g1.finalScore(), 50, 75);
+	}
+
+}
