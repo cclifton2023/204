b6e3d65c531ca13692ecc50aae8c9e8e071a7859
cclifton2023 <92695770+cclifton2023@users.noreply.github.com>
2023-01-31 10:13:00 -0500

    Add files via upload

---
+++ Generic Lab/DataSetGen.java
@@ -0,0 +1,34 @@
+package lab3;
+
+public class DataSetGen <G extends Measurable>
+{
+	private double sum;
+	private G maximum;
+	private int count;
+	
+	public DataSetGen()
+	{
+	    sum = 0;
+	    count = 0;
+	    maximum = null;
+	}
+	
+	public void add(G x)
+	{
+	    sum = sum + x.getMeasure();
+	    if (count == 0 || maximum.getMeasure() < x.getMeasure())
+	    	maximum = x;
+	    count++;
+	}
+	public double getAverage()
+	{
+	    if (count == 0) 
+		return 0;
+	    else 
+	    	return sum / count;
+	}
+	public G getMaximum()
+	{ 
+	    return maximum;
+	}
+}
