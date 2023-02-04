4c2c4da7719a028c1d1d54569097ae8afa3706ef
cclifton2023 <92695770+cclifton2023@users.noreply.github.com>
2023-02-04 12:09:33 -0500

    Add files via upload

---
+++ Assignment 1/NoSpecialCharacterException.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+public class NoSpecialCharacterException extends Exception
+{
+	public NoSpecialCharacterException()
+	{
+		super("The password must contain at least one special character.");
+	}
+}
+
