d1a564c573ce12abaae1b3e3718d9ace19842d20
cclifton2023 <92695770+cclifton2023@users.noreply.github.com>
2023-02-04 14:03:15 -0500

    Delete WeakPasswordException.java

--- Assignment 1/WeakPasswordException.java
+++
@@ -1,9 +0,0 @@
-package assignment1;
-
-public class WeakPasswordException extends Exception
-{
-	public WeakPasswordException()
-	{
-		super("The password is OK but weak - it contains fewer than 10 characters.");
-	}
-}
